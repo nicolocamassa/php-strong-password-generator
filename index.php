7be795e7cde058fd86abc631730369c722393918
nicolocamassa <camassanicolo03@gmail.com>
2024-01-26 15:14:44 +0100

    [UPDATE] input form added for password length

--- index.php
+++ index.php
@@ -1,14 +1,35 @@
-<?php 
+<?php
+    function generatePassword(){
 
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Password Generator</title>
 </head>
+
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <form action="index.php" method="GET" class="mt-3 ms-0 p-0">
+                <input class="form-control" id="passwordLength" name="passwordLength" type="number" placeholder="Lunghezza password">
+                <button type="submit" class="btn btn-success mt-3 m-0">Success</button>
+            </form>
+        </div>
+    </div>
+    <div class="result">
+
+    </div>
+
+    <!-- TODO: 
+         Ricavare da una form la lunghezza della password da generare [GET] 
+         Passare il dato ad una funzione che servirà alla generazione
+    -->
 </body>
+
 </html>
